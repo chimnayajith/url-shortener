7f7a23661c76606033c1471fd946dc95e6e746cb
chimnayajith <chinmayajith30@gmail.com>
2024-09-02 16:24:56 +0530

    feat: register model to admin panel

--- shorten/admin.py
+++ shorten/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ShortenedUrl
 
-# Register your models here.
+@admin.register(ShortenedUrl)
+class ShortenedUrlAdmin(admin.ModelAdmin):
+    list_display = ('original_url', 'short_code', 'long_code', 'created_at')
+    search_fields = ('original_url', 'short_code', 'long_code')
+    list_filter = ('created_at',)
